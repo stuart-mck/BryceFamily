894da62ab9343efe99c9f0ba98a50f754d1c4b50
stuart.mckenzie <stuart.mckenzie@edgeloyalty.com.au>
2018-02-18 10:54:54 +1100

    create unions from child references
    add healthcheck for load balancer

--- src/BryceFamily.Repo.Core/Model/Union.cs
+++ src/BryceFamily.Repo.Core/Model/Union.cs
@@ -6,15 +6,17 @@ namespace BryceFamily.Repo.Core.Model
     [DynamoDBTable("union")]
     public class Union : Entity<Guid>
     {
-        public int PartnerID { get; set; }
+        public int? PartnerID { get; set; }
 
-        public int Partner2ID { get; set; }
+        public int? Partner2ID { get; set; }
 
-        public string Members { get; set; }
+        //public string Members { get; set; }
 
         public DateTime? MarriageDate { get; set; }
 
         public DateTime? DivorceDate { get; set; }
+
+        public bool IsEnded { get; set; }
 
     }
 }
