79f019db62d857920bad4170105fe16d5ff67316
stuart.mckenzie <stuart.mckenzie@edgeloyalty.com.au>
2019-01-28 19:42:01 +1100

    lets use host mode

--- src/BryceFamily.Web.MVC/Program.cs
+++ src/BryceFamily.Web.MVC/Program.cs
@@ -15,7 +15,7 @@ namespace BryceFamily.Web.MVC
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseUrls("http://*:4000")
+                .UseUrls("http://*:4000", "http://*:80", "http://*:443")
                 .Build();
     }
 }
